ca128cde2ae0dcb4460289650c269017ccb80cdb
rafaelsotavares@gmail.com <rafaelsotavares@gmail.com@a8bea8bc-fc55-46d4-f45e-a4bccc390594>
2013-02-07 14:51:52 +0000

    o Site já esta no ar, estou atualizado a unidade de persistencia com novas configurações. nome do banco rafaelst_pibid >>>usuario: rafaelst_pibid01 >>>senha: pibido1 >>> local: quati.net:3306/rafaelst_pibid

--- trunk/PIBID-WEB/src/java/br/ufra/utilitario/ConnectionFactory.java
+++ trunk/PIBID-WEB/src/java/br/ufra/utilitario/ConnectionFactory.java
@@ -21,7 +21,7 @@ public class ConnectionFactory {
             if (conn == null || conn.isClosed()) {
                 System.out.println("<<<<<<<<<<<<<<<<<<CONEXAO BD RELATORIOS>>>>>>>>>>>>>>>>>>>");
                 conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/pibid", "root", "12345");
+                        "jdbc:mysql://quati.net:3306/rafaelst_pibid", "rafaelst_pibid01", "pibido1");
                   //      "jdbc:mysql://localhost:3306/lacibufr_pibid", "lacibufr_root", "831645");
                 return conn;
             }
