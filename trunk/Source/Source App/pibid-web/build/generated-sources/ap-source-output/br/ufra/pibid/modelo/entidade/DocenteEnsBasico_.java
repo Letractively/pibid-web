3994cca1ad4cd3d318b8a923e9da2ccd77a4fe32
stelio.correa@gmail.com <stelio.correa@gmail.com@a8bea8bc-fc55-46d4-f45e-a4bccc390594>
2013-03-03 13:17:02 +0000

    Atualizacao dos arquivos de configuracao do projeto.

--- trunk/Source/Source App/pibid-web/build/generated-sources/ap-source-output/br/ufra/pibid/modelo/entidade/DocenteEnsBasico_.java
+++ trunk/Source/Source App/pibid-web/build/generated-sources/ap-source-output/br/ufra/pibid/modelo/entidade/DocenteEnsBasico_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-02-28T17:23:42")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-03T09:34:19")
 @StaticMetamodel(DocenteEnsBasico.class)
 public class DocenteEnsBasico_ { 
 
--- trunk/Source/Source App/pibid-web/build/generated-sources/ap-source-output/br/ufra/pibid/modelo/entidade/ResponsavelAtividadePK_.java
+++ trunk/Source/Source App/pibid-web/build/generated-sources/ap-source-output/br/ufra/pibid/modelo/entidade/ResponsavelAtividadePK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-02-28T17:23:42")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-03T09:34:20")
 @StaticMetamodel(ResponsavelAtividadePK.class)
 public class ResponsavelAtividadePK_ { 
 
