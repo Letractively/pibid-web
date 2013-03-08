d1ba2bcf5a3bfdd86f09f6798fbe76b565d43ffe
marcosdjufra <marcosdjufra@a8bea8bc-fc55-46d4-f45e-a4bccc390594>
2013-03-08 19:58:51 +0000

    Implementação do cadastro de atribuição de atividades.
    Eu fiz o cadastro de bolsista e aluno colaborador,falta o de professor colaborador e supervisor.É só seguir os outros.
    
    O erro acontecia porque a tabela é composta de 4 chaves estrangeiras e por mais que os campos não são obrigatórios as chaves fazem referência a outras tabelas e por isso não podem ser 0.
    
    Esse fim de semana acho q não vou mecher nele pois tenho que cuidar do meu TCC,mas com a solução deste algoritmo acho que agora estamos perto de concluir o software.

--- Source App/pibid-web/src/java/br/ufra/pibid/modelo/controle/bean/AtividadeAlunoBolsistaBean.java
+++ Source App/pibid-web/src/java/br/ufra/pibid/modelo/controle/bean/AtividadeAlunoBolsistaBean.java
@@ -1,7 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.ufra.pibid.modelo.controle.bean;
 
+import br.ufra.pibid.modelo.entidade.AlunoColaborador;
 import br.ufra.pibid.modelo.entidade.Atividade;
-import br.ufra.pibid.modelo.entidade.Bolsista;
 import br.ufra.pibid.modelo.entidade.Discente;
 import br.ufra.pibid.modelo.entidade.ProfessorColaborador;
 import br.ufra.pibid.modelo.entidade.ResponsavelAtividade;
@@ -21,8 +25,8 @@ import org.primefaces.model.DualListModel;
  */
 @ManagedBean
 @RequestScoped
-public class AtividadeAlunoColaboradorBean {
-
+public class AtividadeAlunoBolsistaBean {
+    
     private ResponsavelAtividadePK respPK;
     private DiscenteRN rn = new DiscenteRN();
     private ResponsavelAtividade responsavelatividade = new ResponsavelAtividade();
@@ -31,74 +35,74 @@ public class AtividadeAlunoColaboradorBean {
     private List<Discente> estaonoprojeto = new ArrayList<Discente>();
     private List<Discente> naoestaonoprojeto = new ArrayList<Discente>();
     private DualListModel<Discente> discentes;
-
+    
     public ResponsavelAtividade getResponsavelatividade() {
         return responsavelatividade;
     }
-
+    
     public void setResponsavelatividade(ResponsavelAtividade responsavelatividade) {
         this.responsavelatividade = responsavelatividade;
     }
-
+    
     public Atividade getAtividade() {
         return atividade;
     }
-
+    
     public void setAtividade(Atividade atividade) {
         this.atividade = atividade;
     }
-
+    
     public List<Discente> getEstaonoprojeto() {
         return estaonoprojeto;
     }
-
+    
     public void setEstaonoprojeto(List<Discente> estaonoprojeto) {
         this.estaonoprojeto = estaonoprojeto;
     }
-
+    
     public List<Discente> getNaoestaonoprojeto() {
         return naoestaonoprojeto;
     }
-
+    
     public void setNaoestaonoprojeto(List<Discente> naoestaonoprojeto) {
         this.naoestaonoprojeto = naoestaonoprojeto;
     }
-
+    
     public DualListModel<Discente> getDiscentes() {
         return discentes;
     }
-
+    
     public void setDiscentes(DualListModel<Discente> discentes) {
         this.discentes = discentes;
     }
-
+    
     public String listagem() {
         return "/restrito/projeto/listagem.xhtml?faces-redirect=true";
     }
-
+    
     public String novo() {
         responsavelatividade = resrn.novo();
-        return "/restrito/projeto/movimentacao/aluno-colaborador/cadastro2.xhtml";
+        return "/restrito/projeto/movimentacao/bolsista/cadastro2.xhtml";
     }
-
+    
     public String atribuir() {
         return "/restrito/projeto/movimentacao/cadastroAtividade.xhtml";
     }
-
+    
     public String salvar() {
         respPK = new ResponsavelAtividadePK();
         List<Supervisor> docentessupervisores = atividade.getProjeto().getSupervisorList();
         for (Supervisor docenteEnsBasico : docentessupervisores) {
             respPK.setSupervisor(docenteEnsBasico.getDocenteEnsBasico().getId());
-
+            
         }
         List<ProfessorColaborador> docentescolaboradores = atividade.getProjeto().getProfessorColaboradorList();
         for (ProfessorColaborador docenteEnsSuperior : docentescolaboradores) {
             respPK.setDocente(docenteEnsSuperior.getDocenteEnsSuperior().getId());
         }
-        List<Bolsista> bolsista = atividade.getProjeto().getBolsistaList();
-        for (Bolsista bolsista1 : bolsista) {
-            respPK.setBolsista(bolsista1.getDiscente1().getId());
+        List<AlunoColaborador> alunocolaborador = atividade.getProjeto().getAlunoColaboradorList();
+        for (AlunoColaborador aluno : alunocolaborador) {
+            respPK.setDiscente(aluno.getDiscente1().getId());
         }
         estaonoprojeto = discentes.getTarget();
         naoestaonoprojeto = discentes.getSource();
@@ -107,24 +111,24 @@ public class AtividadeAlunoColaboradorBean {
             rn.salvar(discente);
         }
         for (Discente discente : estaonoprojeto) {
-
-            discente.setStatusAtividade(2);
-
+            
+            discente.setStatusAtividade(1);
+            
             respPK.setDiscente(discente.getId());
             respPK.setAtividade(atividade.getId());
-            if (discente.getStatus() == 2) {
-                respPK.setDiscente(discente.getId());
+            if (discente.getStatus() == 1) {
+                respPK.setBolsista(discente.getId());
             }
             responsavelatividade.setResponsavelAtividadePK(respPK);
             rn.salvar(discente);
             resrn.salvar(responsavelatividade);
-
+            
         }
         return atribuir();
     }
-
-    public AtividadeAlunoColaboradorBean() {
-        estaonoprojeto = rn.buscarStatusAtividade(2);
+    
+    public AtividadeAlunoBolsistaBean() {
+        estaonoprojeto = rn.buscarStatusAtividade(1);
         System.out.println(estaonoprojeto.size());
         naoestaonoprojeto = rn.buscarStatusAtividade(0);
         discentes = new DualListModel<Discente>(naoestaonoprojeto, estaonoprojeto);
---
+++ Source App/pibid-web/src/java/br/ufra/pibid/modelo/controle/bean/AtividadeSupervisorBean.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufra.pibid.modelo.controle.bean;
+
+/**
+ *
+ * @author marcos
+ */
+public class AtividadeSupervisorBean {
+    
+}
