b7d48e9986e5d8c7cce7c684645fbae57461e949
marcosdjufra <marcosdjufra@a8bea8bc-fc55-46d4-f45e-a4bccc390594>
2013-03-08 19:58:51 +0000

    Implementação do cadastro de atribuição de atividades.
    Eu fiz o cadastro de bolsista e aluno colaborador,falta o de professor colaborador e supervisor.É só seguir os outros.
    
    O erro acontecia porque a tabela é composta de 4 chaves estrangeiras e por mais que os campos não são obrigatórios as chaves fazem referência a outras tabelas e por isso não podem ser 0.
    
    Esse fim de semana acho q não vou mecher nele pois tenho que cuidar do meu TCC,mas com a solução deste algoritmo acho que agora estamos perto de concluir o software.

---
+++ trunk/Source App/pibid-web/src/java/br/ufra/pibid/modelo/controle/util/Teste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufra.pibid.modelo.controle.util;
+
+import br.ufra.pibid.modelo.entidade.ResponsavelAtividade;
+import br.ufra.pibid.modelo.rn.ResponsavelAtividadeRN;
+
+/**
+ *
+ * @author marcos
+ */
+public class Teste {
+    
+    public static void main(String[] args) {
+        ResponsavelAtividade responsavelatividade = new ResponsavelAtividade();
+        ResponsavelAtividadeRN resprn = new ResponsavelAtividadeRN();
+    }
+    
+}
